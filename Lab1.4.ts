3ef696eaba56c0143e7ce688aea0e1e8d6ed240a
Supeephat <684245023@webmail.ac.th>
2026-06-17 16:43:06 +0700

    Lab1_4

---
+++ Lab1.4.ts
@@ -0,0 +1,10 @@
+let GrossIncome: number =50000;
+let TaxRate: number =7;
+let TaxPaid: number=3500;
+
+console.log("--- Tax Calculation /report ---");
+console.log("Gross Income: $%d",GrossIncome);
+console.log("Tax Rate: %d%",TaxRate);
+console.log("Tax Paid: $%d",TaxPaid);
+console.log("Net Income after Tax: $%d",GrossIncome-TaxPaid);
+console.log("-------------------------------");
