d63ea3c2d0e5916fda5b28afb45f87418170dad0
Supeephat <684245023@webmail.ac.th>
2026-06-17 17:03:52 +0700

    Lab1_4

--- Lab1.4.ts
+++ Lab1.4.ts
@@ -1,10 +1,10 @@
 let GrossIncome: number =50000;
 let TaxRate: number =0.07;
-let TaxPaid: number = 3500;
+let Total: number =GrossIncome*TaxRate;
 
 console.log("--- Tax Calculation /report ---");
 console.log("Gross Income: $%d",GrossIncome);
-console.log("Tax Rate: %d%",TaxRate);
+console.log("Tax Rate: 7%");
 console.log("Tax Paid: $%d",GrossIncome*TaxRate);
-console.log("Net Income after Tax: $%d",GrossIncome-TaxPaid);
+console.log("Net Income after Tax: $%d",GrossIncome-Total);
 console.log("-------------------------------");
