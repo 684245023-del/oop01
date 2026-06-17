a6fa392760e6471239bb28e35ec6980670134756
Supeephat <684245023@webmail.ac.th>
2026-06-17 16:13:16 +0700

    add student

--- index.html/studet.ts
+++
@@ -1,2 +0,0 @@
-let fname: string = "Supeephat";
-console.log(fname);
---
+++ studet.ts
@@ -0,0 +1,20 @@
+let fname: string = "Supeephat";
+let lname: string = "Srianan";
+let course: string[] = ["OOP","Dtabase","Web"];
+let mark: number[] = [80,75,70];
+let pass: boolean = true;
+console.log("Hello %s %s",fname,lname);
+console.log("ลงทะเบียนเรียนวิชา",course)
+console.log("ได้คะเเนน",mark);
+console.log("คุณสอบ,pass");
+console.log("คุณสอบได้ %d คะเเนน วิชา %s",mark[0],course[1]);
+
+
+let mark1: number= 75;
+let mark2: number= 80;
+
+let total: number= mark[0]+mark[1]+mark[2];
+let avg: number= total/3;
+console.log("คะเเนนรวม",total);
+console.log("คะเเนนเฉลี่ย",avg);
+
